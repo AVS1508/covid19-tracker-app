92fd2495a33400660aa615c1f4b6f87a61ec930a
AVS1508 <aditya.11.present@gmail.com>
2020-05-11 14:42:33 +0530

    initial PWA commit

---
+++ build/precache-manifest.789cffcbd09af1ebf99d5ca780d8954e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4244c8f5ce0882dbd39c9d7255988f07",
+    "url": "/covid19-tracker-app/index.html"
+  },
+  {
+    "revision": "23d9870dafd0b9f5e81d",
+    "url": "/covid19-tracker-app/static/css/main.fb841060.chunk.css"
+  },
+  {
+    "revision": "3b3cf9b58b132c036f6b",
+    "url": "/covid19-tracker-app/static/js/2.bd86369f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker-app/static/js/2.bd86369f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d9870dafd0b9f5e81d",
+    "url": "/covid19-tracker-app/static/js/main.147ab372.chunk.js"
+  },
+  {
+    "revision": "66405692f9e1b0f8000d",
+    "url": "/covid19-tracker-app/static/js/runtime-main.51e3cf8b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-app/static/media/image.d7265326.png"
+  }
+]);
